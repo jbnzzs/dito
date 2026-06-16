734f8a8f13c44452d6dc0e6df559eeea4e1dc265
João <joao.bueno@scriba.com.br>
2026-06-16 10:06:41 -0300

    Tela de login: split-screen com logo, cards de funcionalidades e esqueci minha senha

--- config/settings.py
+++ config/settings.py
@@ -127,3 +127,8 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Autenticação
+LOGIN_URL = "/login/"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/login/"
